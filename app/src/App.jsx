c052a9a615cc1cba1d58c2c5ded5ab65cc434b6e
karthicknla27 <beepkarthiraj1997@gmail.com>
2023-10-17 09:31:47 +0530

    local storage add

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,39 +1,53 @@
-import { useReducer } from "react";
+import { useReducer, useEffect } from "react";
 import AddTodo from "./compoents/AddTodo";
 import TodoList from "./compoents/TodoList";
 
 export default function Todo() {
-  const [todos, dispatch] = useReducer(todosReducer, []);
+  const initialTodos = JSON.parse(localStorage.getItem("todos")) || [];
+  const [todos, dispatch] = useReducer(todosReducer, initialTodos);
 
   function todosReducer(todos, action) {
     switch (action.type) {
       case "added": {
-        return [
-          ...todos,
-          {
-            id: new Date().getTime(),
-            text: action.text,
-            done: false,
-          },
-        ];
+        const newTodo = {
+          id: new Date().getTime(),
+          text: action.text,
+          done: false,
+        };
+        const updatedTodos = [...todos, newTodo];
+        localStorage.setItem("todos", JSON.stringify(updatedTodos));
+        return updatedTodos;
       }
       case "changed": {
-        return todos.map((todo) => {
+        const updatedTodos = todos.map((todo) => {
           if (todo.id === action.todo.id) {
             return action.todo;
           } else {
             return todo;
           }
         });
+        // Save updated todos to local storage
+        localStorage.setItem("todos", JSON.stringify(updatedTodos));
+        return updatedTodos;
       }
       case "deleted": {
-        return todos.filter((todo) => todo.id !== action.id);
+        const updatedTodos = todos.filter((todo) => todo.id !== action.id);
+        // Save updated todos to local storage
+        localStorage.setItem("todos", JSON.stringify(updatedTodos));
+        return updatedTodos;
       }
       default: {
         throw Error("Unknown action: " + action.type);
       }
     }
   }
+
+  // Clear local storage when the component is unmounted
+  useEffect(() => {
+    return () => {
+      localStorage.removeItem("todos");
+    };
+  }, []);
 
   function handleAddTask(text) {
     dispatch({
@@ -58,7 +72,7 @@ export default function Todo() {
 
   return (
     <>
-      <h1>todo</h1>
+      <h1>Todo</h1>
       <AddTodo onAddTask={handleAddTask} />
       <TodoList todos={todos} onEdit={handleEdit} onDelete={handleDelete} />
     </>
